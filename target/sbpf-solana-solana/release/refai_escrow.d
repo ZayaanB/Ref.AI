fe01ff578edec635152f7417ffc07af0488c6a04
Zayaan Bhanwadia <92405516+ZayaanB@users.noreply.github.com>
2026-06-13 22:07:29 -0400

    Delete target directory

--- target/sbpf-solana-solana/release/refai_escrow.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/ryany/OneDrive/Desktop/RefAI_FrontEnd/target/sbpf-solana-solana/release/refai_escrow.so: /mnt/c/Users/ryany/OneDrive/Desktop/RefAI_FrontEnd/programs/refai_escrow/src/lib.rs
